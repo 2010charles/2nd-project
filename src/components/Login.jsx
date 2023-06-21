c5311bb639c9fdaea2195a3d379962ad6df69cd4
2010charles <charlesmungai2010@gmail.com>
2023-06-21 12:07:31 +0300

    added content on login and register file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function Login() {
+  const schema = yup.object().shape({
+    email: yup.string().email().required("Email is required"),
+    password: yup.string().matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{4,}$/
+      ,
+      'set strong password'
+    ).required("Password is required"),
+    
+  });
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h2>log in page</h2>
+    <div className="form">
+      <form className="formElement"
+        onSubmit={handleSubmit(onSubmit)}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+      <div className='registerpart loginpart'>
+        <h6>Login</h6>
+      </div>
+        <div className="emai">
+          <input type="text" placeholder="Email..." {...register("email")} />
+          <p>{errors.email?.message}</p>
+        </div>
+        <div className="password">
+          <input type="password" placeholder="Password..." {...register("password")} />
+          <p>{errors.password?.message}</p>
+        </div>
+
+        <input className='Register Login' type="submit" value="Login" style={{ width: "50%" }} />
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-
+import './register.css'
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -7,11 +7,11 @@ function Register() {
   const schema = yup.object().shape({
     fullName: yup.string().required("Full name is required"),
     email: yup.string().email().required("Email is required"),
-    age: yup.number("Age must be a number").positive("Age must be a positive number").required("Age is required"),
+    age: yup.number("number").positive("+").required("Age is required"),
     password: yup.string().matches(
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{4,}$/
       ,
-      'password must contain at least 4 characters,uppercase,lowercase,number and one special case character'
+      'set strong password'
     ).required("Password is required"),
     confirmPassword: yup
       .string()
@@ -25,8 +25,8 @@ function Register() {
     console.log(data);
   };
   return (
-    <>
-      <form
+    <div className="form">
+      <form className="formElement"
         onSubmit={handleSubmit(onSubmit)}
         style={{
           display: "flex",
@@ -34,30 +34,33 @@ function Register() {
           alignItems: "center",
         }}
       >
-        <>
+      <div className='registerpart'>
+        <h6>Register</h6>
+      </div>
+        <div className="name">
           <input type="text" placeholder="Full name" {...register("fullName")} />
           <p>{errors.fullName?.message}</p>
-        </>
-        <>
+        </div>
+        <div className="emai">
           <input type="text" placeholder="Email..." {...register("email")} />
           <p>{errors.email?.message}</p>
-        </>
-        <>
+        </div>
+        <div className="age">
           <input type="number" placeholder="Age..." {...register("age")} />
           <p>{errors.age?.message}</p>
-        </>
-        <>
+        </div>
+        <div className="password">
           <input type="password" placeholder="Password..." {...register("password")} />
           <p>{errors.password?.message}</p>
-        </>
-        <>
+        </div>
+        <div className="ConfirmPassward">
           <input
             type="password" placeholder="Confirm Password..."  {...register("confirmPassword")} />
           <p>{errors.confirmPassword?.message}</p>
-        </>
-        <input type="submit" value="Submit" style={{ width: "50%" }} />
+        </div>
+        <input className='Register' type="submit" value="Register" style={{ width: "50%" }} />
       </form>
-    </>
+    </div>
   );
 }
 
